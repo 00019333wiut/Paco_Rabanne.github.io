da5e68de1f6a809569facf3fe71c32d1b9cef0bd
00019333wiut <147713159+00019333wiut@users.noreply.github.com>
2023-11-23 22:08:02 +0500

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,4 @@
+I created design of this web page by myself, this web site is about beauty cosmetics, it has 5 pages which are home,about us , products, contact us and vlog. This website uses JS functions on buttons and forms. I apologize that my site does not have "responsive" feature.
+
+
+Link to the website: 
---
+++ script.js
@@ -0,0 +1,32 @@
+// for buttons
+
+function redirectToWebsite() {
+    window.location.href = "product.html";
+    }
+
+ 
+
+
+//FORM START
+function register() {
+    // Get form values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Perform form validation
+    if (name === '' || email === '' || password === '') {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    // Display success message
+    const successMessage = document.getElementById('success-message');
+    successMessage.textContent = 'Thank you for registering!';
+    window['alert']('Registered successfully');
+    // Reset form fields
+
+     document.getElementById('registration-form').reset();
+
+    
+  }
